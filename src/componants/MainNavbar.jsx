60e78c02795da1a3d1f922efd15a80ca155e5ae9
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2021-11-30 11:04:25 +0700

    fixed hrefs and add design

--- src/componants/MainNavbar.jsx
+++ src/componants/MainNavbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { Outlet } from 'react-router';
+import "./mainNavbar.css"
 
 const mapStateToProps = (state) => {
     return state
@@ -11,20 +12,20 @@ const MainNavbar = (props) => {
     console.log(props.activeUser)
     return(
         <React.Fragment>
-            <Navbar bg="light" expand="lg" variant="light">
-                <Container>
+            <Navbar className="main-navbar"  expand="lg" variant="light">
+                <Container className="mw-100">
                     <Navbar.Brand href="/">HOA Systems</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/main-dashboard">Dashboard</Nav.Link>
+                            <Nav.Link href="/main/dashboard">Dashboard</Nav.Link>
                             {(props.activeUser.activeUser.isCommitteeMember) ? 
-                            <Nav.Link href="/tenants">Tenants</Nav.Link> : null}
-                            <Nav.Link href="/messages">Messages</Nav.Link>
-                            <Nav.Link href="/issues">Issues</Nav.Link>
-                            <Nav.Link href="/voting">Voting</Nav.Link>
+                            <Nav.Link href="/main/tenants">Tenants</Nav.Link> : null}
+                            <Nav.Link href="/main/messages">Messages</Nav.Link>
+                            <Nav.Link href="/main/issues">Issues</Nav.Link>
+                            <Nav.Link href="/main/voting">Voting</Nav.Link>
                         </Nav>
-                        <Nav className="ml-auto">
+                        <Nav className="justify-content-end flex-grow-1">
                             <Nav.Link href="/">Logout</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>

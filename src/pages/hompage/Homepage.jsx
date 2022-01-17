690320724f0353a671b69db43fce961267cfe774
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2022-01-17 10:20:15 +0700

    add some text to homepage

--- src/pages/hompage/Homepage.jsx
+++ src/pages/hompage/Homepage.jsx
@@ -5,6 +5,7 @@ import HomepageCard from './HomepageCard'
 import { Container } from 'react-bootstrap'
 
 function Homepage (){
+
     return (
         <React.Fragment>
             <HomepageNavbar/>
@@ -12,17 +13,17 @@ function Homepage (){
                 <HomepageCard
                 imgSrc="https://i.pinimg.com/736x/8d/1a/c2/8d1ac2d98b1ee557a8ec0cbd8832ef48.jpg"
                 CardTitle="COMMUNICATION"
-                cardText="Stay tuned and up to date with "
+                cardText="Stay tuned and up to date with everything that is happaning around your home."
                 />
                 <HomepageCard
                 imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA1R35vfV640kgf1j1j2OXLCwMMeq6BwMqBhnw6abcXmLkZ2PnwhKadhsQrSg2mGLFeMI&usqp=CAU"
                 CardTitle="TRANSPARENCY"
-                cardText="Always be informed with decisions making regarding changes and improvements and committee money spendings"
+                cardText="Always be informed with decisions making regarding changes, improvements and committee money spendings"
                 />
                 <HomepageCard
                 imgSrc="https://png.pngtree.com/png-vector/20190626/ourlarge/pngtree-adoption-and-communitynetwork-and-social-icon-design-png-image_1513774.jpg"
                 CardTitle="COMMUNITY"
-                cardText="Be part of "
+                cardText="The people that srrounds you create a unique community, be part of it and make your life happier by getting to know them."
                 />
             </Container>
         </React.Fragment>

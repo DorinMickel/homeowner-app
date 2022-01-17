7b4df53567992ff60287421b37a33607ef0fe29e
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2022-01-17 10:22:46 +0700

    add properties to ButtonComp

--- src/componants/ButtonComp.jsx
+++ src/componants/ButtonComp.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Button } from 'react-bootstrap';
 
 const ButtonComp = (props) => {
+    const btnColor = props.btnColor ? props.btnColor : "lightcoral"
     return (
-        <Button style={{backgroundColor: "lightcoral", border: 0}} 
+        <Button style={{backgroundColor: btnColor, border: 0}} 
         onClick={props.handleClick} 
-        variant="primary" type="button">
+        type={props.btnType ? props.btnType : "button"} disabled={props.disable}>
             {props.btnText}
         </Button>
     )
